0749e27866a2fae5dff44ea3735e659a3c04e477
Jon-Michael Deldin <dev@jmdeldin.com>
2008-07-15 16:44:09 -0600

    Don't try to insert a form if the existing is empty.

--- jmd_dashboard.php
+++ jmd_dashboard.php
@@ -96,7 +96,7 @@ function jmd_dashboard($event, $step)
         $GLOBALS['pretext'] = array('id' => '', 'q' => '',);
     }
     $contents = safe_field("Form", "txp_form", "name = 'jmd_dashboard'");
-    if (empty($contents))
+    if (!isset($contents))
     {
         $contents = <<<FORM
 <h1 style="text-align:center">
